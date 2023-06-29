18a04a2ce882b0cfc672bf50ec3233bce50ffb28
Vlad Buzilov <vladbuzilov23@gmail.com>
2023-06-29 15:50:30 +0300

    Initial commit

---
+++ DOUWebSiteTestSelenium/MainPageTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using DOUWebSiteTestSelenium.PageObjects;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace DOUWebSiteTestSelenium
+{
+    [TestFixture]
+    public class MainPageTests
+    {
+        private IWebDriver _webDriver;
+
+        [OneTimeSetUp] public void OneTimeSetUtp()
+        {
+            _webDriver = new ChromeDriver();
+        }
+
+        [SetUp] public void SetUp()
+        {
+            _webDriver.Manage().Cookies.DeleteAllCookies();
+            _webDriver.Navigate().GoToUrl(Settings.URLAddress);
+            _webDriver.Manage().Window.Maximize();
+        }
+
+        [Test] public void MainPageTabTest()
+        {
+            var mainPageTab = new MainPageObject(_webDriver);
+            mainPageTab.FirstWork().BackToMain();
+        }
+
+        [TearDown] public void TearDown()
+        {
+            _webDriver.Quit();
+        }
+    }
+}
---
+++ DOUWebSiteTestSelenium/Settings.cs
@@ -0,0 +1,7 @@
+namespace DOUWebSiteTestSelenium
+{
+    public static class Settings
+    {
+        public const string URLAddress = "https://dou.ua";
+    }
+}
